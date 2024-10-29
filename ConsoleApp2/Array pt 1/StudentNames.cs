cf1cb8680b50b3e575f29339a535566275f3e1f3
mvakiashvili <mvakiashvili@LHO10076727>
2024-10-29 20:42:22 +0400

    students

---
+++ ConsoleApp2/Array pt 1/StudentNames.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml.Linq;
+
+internal class StudentsArray
+
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the Name");
+
+        string [] students = new string[3];
+
+
+        
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            string name = (Console.ReadLine());
+            students[i] = name;
+        }
+        Console.WriteLine("");
+
+        Console.WriteLine("Studentlist");
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.WriteLine(students[i]);
+        }
+
+
+
+    }
+}
