09d66231cc50f883575c264e9f9b9241b52d3806
mvakiashvili <mvakiashvili@LHO10076727>
2024-11-09 23:41:19 +0400

    ბიბლიოთეკებიი

---
+++ ConsoleApp2/Library/UseLibrary.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Library
+{
+    internal class UseLibrary
+    {
+        static void Main()
+        {
+            Library library = new Library();
+
+            library.AddBook(new book("Selected Short Stories", "E.A Poe", 2020));
+            library.AddBook(new book("At the mountains of Madness", "H.P. Lovecraft", 1936));
+            library.AddBook(new book("Pride And Prejudice", "Jane Austin", 1813));
+
+
+            Console.WriteLine("Books in the library");
+            library.PrintBooks();
+
+            Console.WriteLine($"\n Amount of Books in Library: {library.Count}");
+
+            Console.WriteLine("\n Search 'Selected Short Stories':");
+
+            var foundBooks = library.FindBook("Selected Short Stories");
+            foreach (var book in foundBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+            Console.WriteLine("\n Delete Book 'Selected Short Stories' :");
+            library.RemoveBook("At the mountains of Madness");
+
+            Console.WriteLine("\n Books left in library");
+            library.PrintBooks();
+        }
+    }
+}
+
