d8ff738e789c334d3431f587c9b4881c3a58e8b8
mvakiashvili <mvakiashvili@LHO10076727>
2024-11-17 21:38:22 +0400

    updates

---
+++ ConsoleApp2/MathHelper/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace MathHelper
+{
+    public class MathOperations
+    {
+        static void Main ()
+        {
+            int a = 24;
+            int b = 2;
+
+        Console.WriteLine($"Sum of {a} and {b} is " + MathHelper.Add(a, b));
+        Console.WriteLine($"Subtract of {a} and {b} is" + MathHelper.Subtract(a, b));
+        Console.WriteLine($"Multiplication of {a} and {b} is "+ MathHelper.Multiply(a, b));
+        Console.WriteLine($"Division of {a} and {b}is" + MathHelper.Divide(a, b));
+        }
+
+    }
+}
